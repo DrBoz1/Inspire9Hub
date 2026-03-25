0755570beeea2148d909b6a6a5c7dae4070faed8
Dr.Boz <hesamstark99@gmail.com>
2026-03-26 02:01:10 +0800

    supabase client library and supabase ssr added, created the necessary files, will work further on server actions later

---
+++ app/(auth)/actions.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { createClient } from "@supabase/supabase-js";
+import { redirect } from "next/navigation";
+
+export async function login(formData: FormData) {
+  const supabase = await createClient();
+
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+}
