943e46b9ff74bb176ae4c1f2896bbee3fb28e9b6
Dr.Boz <hesamstark99@gmail.com>
2026-03-27 01:40:57 +0800

    import for actions file fixed, error gone

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
 export async function login(formData: FormData) {
