d754b64e4f4579045fb4017f3d5659d0e68323ee
Dr.Boz <hesamstark99@gmail.com>
2026-03-27 01:24:27 +0800

    middleware done

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -10,4 +10,32 @@ export async function login(formData: FormData) {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
   };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    return redirect("/login?error=Could not authenticate user");
+  }
+
+  return redirect("/dashboard"); //placeholder for when the dashboard is created
+}
+
+export async function signUp(formData: FormData) {
+  const supabase = await createClient();
+
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+    options: {
+      data: {
+        full_name: formData.get("name") as string,
+      },
+    },
+  };
+
+  const { error } = await supabase.auth.signUp(data);
+  if (error) {
+    return redirect("signup?error=Could not verify user");
+  }
+  return redirect("login/message=Check email to confirm registration");
 }
---
+++ middleware.ts
@@ -0,0 +1,41 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({ request: { headers: request.headers } });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name, value, options) {
+          request.cookies.set({ name, value, ...options });
+        },
+        remove(name, options) {
+          request.cookies.set({ name, value: "", ...options });
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (request.nextUrl.pathname.startsWith("/admin") && user) {
+    const { data: profile } = await supabase
+      .from("profiles")
+      .select("role")
+      .eq("id", user.id)
+      .single();
+
+    if (profile?.role !== "admin" && profile?.role !== "super_admin") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+  }
+  return response;
+}
