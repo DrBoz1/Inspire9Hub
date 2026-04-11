dbdf08ec324622afdfcbca60b5fd8919af22f6ab
Dr.Boz <hesamstark99@gmail.com>
2026-04-11 17:13:08 +0800

    added a middle state between nothing and finished for induction and member status

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,11 @@
 export const INDUCTION_STATUS = {
   PENDING: "Pending",
+  SUBMITTED: "Submitted",
   COMPLETE: "Complete",
 };
 
 export const MEMBER_STATUS = {
   ACTIVE: "Active",
   INACTIVE: "Inactive",
+  SUSPENDED: "Suspended",
 };
